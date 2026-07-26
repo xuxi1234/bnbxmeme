4f9a66d081de15ac2b51be7320ae439be8166b40
xuxi1234 <104724831+xuxi1234@users.noreply.github.com>
2026-07-27 04:41:59 +0800

    Add auto-liquidity V2: update packages/contracts/src/interfaces/IPancakeV2.sol

--- packages/contracts/src/interfaces/IPancakeV2.sol
+++ packages/contracts/src/interfaces/IPancakeV2.sol
@@ -13,6 +13,14 @@ interface IPancakeV2Router {
         address to,
         uint256 deadline
     ) external payable returns (uint256 amountToken, uint256 amountETH, uint256 liquidity);
+
+    function swapExactTokensForETHSupportingFeeOnTransferTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external;
 }
 
 interface IPancakeV2Factory {
